f1142dfde9e07941e222fe650a480b0d5d58cbca
fernandovardev <141197580+fernandovardev@users.noreply.github.com>
2024-06-01 14:11:53 -0700

    named import fix

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
-from config import DATABASE_URL 
+from src.config import DATABASE_URL 
 import os
 
 
