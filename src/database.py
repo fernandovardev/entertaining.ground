bf2f2a18c5bece472e1a8a9da36974b4e6e10c9b
fernandovardev <141197580+fernandovardev@users.noreply.github.com>
2024-06-01 14:09:36 -0700

    CORRECT LOAD ENV VARIABLE

--- src/database.py
+++ src/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
+from config import DATABASE_URL 
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", DATABASE_URL = "sqlite:///:memory:"
-)
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
